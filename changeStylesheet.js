2364c29e92f93a80a92af605a815545be38755e3
Mason <mfryk268@gmail.com>
2019-04-24 12:41:25 +0000

    Yeet

--- changeStylesheet.js
+++ changeStylesheet.js
@@ -1,3 +1,4 @@
 function mojave() {
-document.getElementById("lolzor").style.backgroundImage="url('http://cdn.osxdaily.com/wp-content/uploads/2018/06/macos-mojave-night-lightened-r.jpg') !important"
+var body = document.getElementsByTagName('body')[0];
+body.style.backgroundImage = 'url(http://cdn.osxdaily.com/wp-content/uploads/2018/06/macos-mojave-night-lightened-r.jpg)';
 }
