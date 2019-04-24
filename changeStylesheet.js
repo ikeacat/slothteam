e98287e0bababb9c41bd9a23a2c8cb2bc307d6c2
Mason <mfryk268@gmail.com>
2019-04-24 12:22:20 +0000

    Test

---
+++ changeStylesheet.js
@@ -0,0 +1,4 @@
+function mojave() {
+var change = document.querySelector('#lolzor');
+change.style.backgroundImage = "url('https://cdn.osxdaily.com/wp-content/uploads/2018/06/macos-mojave-night-blues-lighter.jpg')";
+}
