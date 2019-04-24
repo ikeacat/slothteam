75094853410ae1ba058d517d9d0d12b2a98e28af
Mason <mfryk268@gmail.com>
2019-04-24 12:37:44 +0000

    Maybe this will work

--- changeStylesheet.js
+++ changeStylesheet.js
@@ -1,3 +1,3 @@
 function mojave() {
-document.getElementById("lolzor").style.backgroundImage="url('http://cdn.osxdaily.com/wp-content/uploads/2018/06/macos-mojave-night-lightened-r.jpg')"
+document.getElementById("lolzor").style.backgroundImage="url('http://cdn.osxdaily.com/wp-content/uploads/2018/06/macos-mojave-night-lightened-r.jpg') !important"
 }
