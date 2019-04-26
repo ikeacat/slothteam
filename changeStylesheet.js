71e33ab0394f6bf80d1b000f1401b7001eea39f5
ikeacat <mfryk98@gmail.com>
2019-04-25 22:37:02 -0500

    Remove changeStylesheet.js

--- changeStylesheet.js
+++
@@ -1,4 +0,0 @@
-function mojave() {
-var body = document.getElementsByTagName('body')[0];
-body.style.backgroundImage = 'url(http://cdn.osxdaily.com/wp-content/uploads/2018/06/macos-mojave-night-lightened-r.jpg)';
-}
