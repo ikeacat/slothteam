02419fb6d000beee1bad5710993e9fb1253af47a
ikeacat <23439786+ikeacat@users.noreply.github.com>
2019-08-13 12:32:37 -0500

    add an image to random

--- js/imageselect.js
+++ js/imageselect.js
@@ -3,9 +3,10 @@ ImageArray[0] = 'pictures/bg-sloth1.jpg';
 ImageArray[1] = 'pictures/bg-sloth2.jpg';
 ImageArray[2] = 'pictures/gallery-only/sloth1.jpeg';
 ImageArray[3] = 'pictures/rand-sloth1.jpg'
+ImageArray[4] = 'pictures/rand-sloth2.jpg'
 
 function getRandomImage() {
-  var num = Math.floor( Math.random() * 4);
+  var num = Math.floor( Math.random() * 5);
   var img = ImageArray[num];
   document.getElementById("randomSlothImage").innerHTML = ('<img src="' + img + '" width="92%">')
 }
