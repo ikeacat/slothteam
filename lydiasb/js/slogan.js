594abb4ef3d816e3bf541a1a4b1a2fd7653e00d3
ikeacat <mfryk268@gmail.com>
2019-09-13 15:01:15 +0000

    Lydia - New Slogans

--- lydiasb/js/slogan.js
+++ lydiasb/js/slogan.js
@@ -5,9 +5,12 @@ SloganArray[1] = "Definitely a basic VSCO girl.";
 SloganArray[2] = "Where's my hydroflask?";
 SloganArray[3] = "Die in a fire";
 SloganArray[4] = "Can you play Roblox?";
+SloganArray[5] = "Sksksksk";
+SloganArray[6] = "VSCO";
+SloganArray[7] = "And i OOP";
 
 function WeGonnaNeedASlogan() {
-  var num = Math.floor( Math.random() * 5);
+  var num = Math.floor( Math.random() * 8);
   var slg = SloganArray[num];
   document.getElementById("randSlogan").innerHTML = ('<p>' + slg + '</p>');
 };
