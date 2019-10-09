82ba85bfb38f01482672bf9e223ca0044c95b6ec
ikeacat <mfryk268@gmail.com>
2019-10-09 08:13:17 -0500

    More VSCO Slogans

--- lydiasb/js/slogan.js
+++ lydiasb/js/slogan.js
@@ -8,9 +8,12 @@ SloganArray[4] = "Can you play Roblox?";
 SloganArray[5] = "Sksksksk";
 SloganArray[6] = "VSCO";
 SloganArray[7] = "And i OOP";
+SloganArray[8] = "We should play Town of Salem";
+SloganArray[9] = "Dropped my hydroflask";
+SloganArray[10] = "And i OOP dropped my hydroflask sksksksk";
 
 function WeGonnaNeedASlogan() {
-  var num = Math.floor( Math.random() * 8);
+  var num = Math.floor( Math.random() * 11);
   var slg = SloganArray[num];
   document.getElementById("randSlogan").innerHTML = ('<p>' + slg + '</p>');
 };
